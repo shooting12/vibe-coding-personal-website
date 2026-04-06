a0bc41818a27e25fee18cc8dde1b587dc7d0842e
Shooting Chuang <shooting729@gmail.com>
2026-04-05 23:45:19 -0700

    Try to modify base reference folder path
    Following the recent deployment, the website is rendering a completely blank white page. Try debugging by changing base path.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vibe-coding-personal-website/',
+  base: './', // Use reference path instead of vibe-coding-personal-website
 })
