4e360d4f21001fedf1d8cd92d226ec4f554ea876
Shooting Chuang <shooting729@gmail.com>
2026-04-05 23:52:24 -0700

    Revert "Try to modify base reference folder path"
    This reverts commit a0bc41818a27e25fee18cc8dde1b587dc7d0842e.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use reference path instead of vibe-coding-personal-website
+  base: '/vibe-coding-personal-website/',
 })
